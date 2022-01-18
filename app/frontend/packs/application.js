25adfc49b26ba6d6d9361ba057a8c1d2cbfabff1
shikushwah <shikushwah@bestpeers.com>
2022-01-18 16:43:19 +0530

    shopping demo

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import "bootstrap"
+import "../stylesheets/application"
+import '../js/bootstrap_js_files.js'  
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
